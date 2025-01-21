dc0c9330319b81e0e5a6c7f75fbbd34ab0ea9fc7
henry <kaynekaynekayne@github.com>
2025-01-21 16:08:57 +0900

    원자력 병원 RTF 삽입, 업데이트 가상 코드 추가

--- app.js
+++ app.js
@@ -22,15 +22,83 @@ async function initializeDB() {
     }
 }
 
+// 검체번호(또는 바코드번호), 검사코드, 검체코드, 검사결과본문(rtf), 전송자ID, 전송시간
+// 연동 방식은 중간테이블에 결과 데이터를 직접 insert, update 해주시면 될 것 같습니다. 
+// 결과 전송 이후 재전송이 가능한 부분에 대해 데이터 관리를 어떻게 할지 검토 부탁 드리며, 이를 위해 필요한 필드가 있으면 회신 부탁 드립니다. 
+
 // 미들웨어 설정
 app.use(express.json()); // JSON 요청 본문을 파싱
 
-// 엔드포인트 설정
+// 엔드포인트 설정`
 app.use('/api/imgLoad', imgLoadRoutes);
 
 // 기본 라우트
 app.get('/', (req, res) => {
     res.send('Welcome to the Express API!');
+});
+
+
+// RTF - 원자력
+app.post('/0033-sendRTF', async (req, res) => {
+    //  검체번호(또는 바코드번호), 검사코드, 검체코드, 검사결과본문(rtf), 전송자ID, 전송시간
+    // barcodeNo, 검사 코드, 검체 코드, rtf 본문, 전송자 ID, 전송시간
+    const { barcodeNo, rtfBody } = req.body;
+
+    if (!barcodeNo || !rtfBody) {
+        res.status(400).json({ error: 'barcodeNo or rtfBody are required' });
+    }
+
+    console.log('barcodeNo', barcodeNo);
+    console.log('rtfBody', rtfBody);
+
+    // 중간테이블 접속 후 insert, update문 필요
+    res.send('Success');
+
+    // 밑의 주석 코드는 oracle DB에 barcodeNo와 rtf를 업데이트 및 삽입 하는 내용입니다. 수정이 필요합니다.
+
+    // let connection;
+
+    // try {
+    //     connection = await oracledb.getConnection();
+
+    //     const checkSQL = `
+    //         SELECT COUNT(*) AS count
+    //         FROM 중간테이블
+    //         WHERE barcodeNo = :barcodeNo
+    //     `;
+    //     const result = await connection.execute(checkSQL, { barcodeNo });
+
+    //     if (result.rows[0].COUNT > 0) {
+    //         const updateSQL = `
+    //             UPDATE 중간테이블
+    //             SET rtfBody = :rtfBody
+    //             WHERE barcodeNo = :barcodeNo
+    //         `;
+
+    //         await connection.execute(updateSQL, { barcodeNo, rtfBody }, { autoCommit: true });
+
+    //         return res.status(200).json({ message: 'Data updated successfully' });
+    //     } else {
+    //         const insertSQL = `
+    //             INSERT INTO 중간테이블 (barcodeNo, rtfBody)
+    //             VALUES (:barcodeNo, :rtfBody)
+    //         `;
+
+    //         await connection.execute(insertSQL, { barocodeNo, rtfBody }, { autoCommit: true });
+    //         return res.status(201).json({ message: 'Data inserted successfully' });
+    //     }
+    // } catch (err) {
+    //     console.error('Error while processing request:', err);
+    //     return res.status(500).json({ error: 'Internal server error' });
+    // } finally {
+    //     if (connection) {
+    //         try {
+    //             await connection.close();
+    //         } catch (closeErr) {
+    //             console.error('연결 종료 중 오류: ', closeErr.message);
+    //         }
+    //     }
+    // }
 });
 
 // 서버 실행
